2dc9f8fa9adfc39c66dd58e112564314f27329ab
Karol Banat <karol.banat@gmail.com>
2022-12-15 13:41:36 +0100

    Simple card animation

--- src/js/director-card.js
+++ src/js/director-card.js
@@ -10,10 +10,37 @@ const handleExpandCardButton = e => {
 	const targetCardId = button.getAttribute('aria-controls');
 	const targetCard = document.querySelector(`#${targetCardId}`);
 	targetCard.toggleAttribute('data-expanded', !wasExpanded);
-	const toShowElements = targetCard.querySelectorAll('[data-hidden="true"]');
-	const toHideElements = targetCard.querySelectorAll('[data-hidden="false"]');
-	toShowElements.forEach(element => element.setAttribute('data-hidden', false));
-	toHideElements.forEach(element => element.setAttribute('data-hidden', true));
+	const hiddenParts = targetCard.querySelectorAll('[data-hidden="true"]');
+	const visibleParts = targetCard.querySelectorAll('[data-hidden="false"]');
+
+	/* function to invoke after showing animation ends */
+	const showHiddenParts = () => {
+		hiddenParts.forEach(part => {
+			part.setAttribute('data-hidden', false);
+			part.classList.add('animation-fade-in');
+			part.addEventListener(
+				'animationend',
+				_ => {
+					part.classList.remove('animation-fade-in');
+				},
+				{ once: true }
+			);
+		});
+	};
+
+	/* after the animation hidden parts are going to be shown */
+	visibleParts.forEach(part => {
+		part.classList.add('animation-fade-out');
+		part.addEventListener(
+			'animationend',
+			_ => {
+				part.classList.remove('animation-fade-out');
+				part.setAttribute('data-hidden', true);
+				showHiddenParts();
+			},
+			{ once: true }
+		);
+	});
 };
 
 directorCardsButtons.forEach(btn => btn.addEventListener('click', handleExpandCardButton));
